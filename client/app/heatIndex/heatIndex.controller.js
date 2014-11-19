d8bf76e3165b7106651a946cb182ea06bd2d4ae3
iwanaga <iwanaga@iij.ad.jp>
2014-11-19 12:30:59 +0900

    added empty line

--- client/app/heatIndex/heatIndex.controller.js
+++ client/app/heatIndex/heatIndex.controller.js
@@ -6,6 +6,7 @@ angular.module('hemsjsApp').controller('HeatIndexCtrl', function ($scope, $http,
     socket.socket.on('ThermoHygroHistory:save', function (thermoHygro) {
         $scope.thermoHygro = thermoHygro;
     });
+
     $http.get('/api/thermoHygroHistories/last').success(function (thermoHygro) {
         $scope.thermoHygro = thermoHygro;
     });
